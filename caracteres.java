b32ef743436071b97d4374f9398d2edbf7a00bd8
EduardoAcostaC <eacostac1700@alumno.ipn.mx>
2023-06-13 09:12:44 -0600

    Ejemplos de declaración de caracteres y strings

---
+++ caracteres.java
@@ -0,0 +1,22 @@
+public class caracteres {
+    public static void main(String[] args) {
+        
+        char caracter = 'a'; //Para definir un char se usan '' 
+        System.out.println(caracter);
+
+        caracter = 65;
+        System.out.println(caracter);
+
+        caracter = 65 + 1;
+        char segundoCaracter = (char) (caracter + 1);
+        System.out.println(segundoCaracter);
+
+        String cadena = "Alura curso online";
+        System.out.println(cadena);
+
+        cadena =  cadena + "2023";
+        System.out.println(cadena);
+
+        
+    }
+}
